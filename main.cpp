0868fbbbce7ea987770405df646819e29bb28ef8
DISCIPLINE55 <ismailmensah055@gmail.com>
2024-07-13 20:36:54 +0000

    This shows how to calculate the shape

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	float lenght, width;
+	cout << " Enter the lenght of the rectangle ",
+	cin >>lenght;
+	cout << " Enter the width of the rectangle ";
+	cin >>width;
+	cout <<" The area of the rectangle is "<<lenght * width<<endl;
+	return 0;
+} 
